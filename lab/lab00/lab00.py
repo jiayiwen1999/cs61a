47be51cd46d800c3b258738509b34d2453f01fbe
jiayiwen1999 <jiayiwen.99@gmail.com>
2021-06-10 11:11:44 +0800

    lab00

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return 1-2+3-4+5*168*2+7*(8+1//2+89//4+7)-9*7+10*11+12+12+12
+
