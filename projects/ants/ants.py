0eeb4bca3ee6e45f64992af7b207d61218cf4ae2
jiayiwen1999 <jiayiwen.99@gmail.com>
2021-06-20 10:44:44 +0800

    phase 1 complete

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -24,7 +24,8 @@ class Place:
         self.entrance = None  # A Place
         # Phase 1: Add an entrance to the exit
         # BEGIN Problem 2
-        "*** YOUR CODE HERE ***"
+        if self.exit != None:
+            self.exit.entrance = self
         # END Problem 2
 
     def add_insect(self, insect):
@@ -141,7 +142,8 @@ class HarvesterAnt(Ant):
 
     name = 'Harvester'
     implemented = True
-    # OVERRIDE CLASS ATTRIBUTES HERE
+    food_cost = 2 
+
 
     def action(self, gamestate):
         """Produce 1 additional food for the colony.
@@ -149,7 +151,7 @@ class HarvesterAnt(Ant):
         gamestate -- The GameState, used to access game state information.
         """
         # BEGIN Problem 1
-        "*** YOUR CODE HERE ***"
+        gamestate.food += 1
         # END Problem 1
 
 
@@ -159,7 +161,7 @@ class ThrowerAnt(Ant):
     name = 'Thrower'
     implemented = True
     damage = 1
-    # ADD/OVERRIDE CLASS ATTRIBUTES HERE
+    food_cost = 3 
 
     def nearest_bee(self, beehive):
         """Return the nearest Bee in a Place that is not the HIVE (beehive), connected to
@@ -168,7 +170,13 @@ class ThrowerAnt(Ant):
         This method returns None if there is no such Bee (or none in range).
         """
         # BEGIN Problem 3 and 4
-        return rANTdom_else_none(self.place.bees) # REPLACE THIS LINE
+        curr_place = self.place
+        while curr_place.bees == []:            
+            if curr_place.entrance.name != 'Hive' :
+                curr_place = curr_place.entrance
+            else:
+                return None
+        return rANTdom_else_none(curr_place.bees)
         # END Problem 3 and 4
 
     def throw_at(self, target):
